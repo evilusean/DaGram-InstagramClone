d0466b620a50d3c4860e3bf07b7c5ee36e276cf2
evilusean <shawntemesy@hotmail.com>
2023-03-16 14:58:45 -0300

    Fixed main

--- dagram_flutter/lib/responsive/responsive_layout_screen.dart
+++ dagram_flutter/lib/responsive/responsive_layout_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_flutter/utils/dimensions.dart';
+import 'package:dagram_flutter/utils/dimensions.dart';
 
 class ResponsiveLayout extends StatelessWidget {
   final Widget webScreenLayout;
