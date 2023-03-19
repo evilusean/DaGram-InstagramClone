b7a78b624b0ab118c9517990774ebf8ee2c0dc9c
evilusean <shawntemesy@hotmail.com>
2023-03-19 18:58:55 -0300

    succesfully ran seed.js and uploaded data to firebase

--- DaGram-React/dagram/src/lib/firebase.js
+++ DaGram-React/dagram/src/lib/firebase.js
@@ -1,10 +1,16 @@
-import Firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth'
+//import Firebase from 'firebase/app';
+//import 'firebase/firestore';
+//import 'firebase/auth'
+//new version of firebase added /compat - fix works
+import Firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth'
+
 
 //Import seed file,
-
-const config {
+//import { seedDatabase } from '../seed';
+//Firebase Config Information
+const config = {
     apiKey: "AIzaSyBv540S56U6-jxxAl8EgeFVzYrXhN4IxRk",
     authDomain: "dagram-react.firebaseapp.com",
     projectId: "dagram-react",
@@ -12,12 +18,11 @@ const config {
     messagingSenderId: "670352114563",
     appId: "1:670352114563:web:ba5dfab4cb4d74a632acb6"
 };
-
+//initialize firebase app
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
 
-//Run seed, only once!
-// seedDatabase(firebase);
-console.log('firebase', firebase);
+//Run seed, only once! Ran successfully, data uploaded to firebase.
+//seedDatabase(firebase);
 
 export { firebase, FieldValue };
