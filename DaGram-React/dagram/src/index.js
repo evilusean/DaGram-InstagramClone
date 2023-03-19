7c6a719577932814edaabcaf6bbe7b654988e699
evilusean <shawntemesy@hotmail.com>
2023-03-19 18:40:19 -0300

    deleted eslint added firebase info

--- DaGram-React/dagram/src/index.js
+++ DaGram-React/dagram/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import FirebaseContext from './context/firebase';
+import { firebase, FieldValue } from './lib/firebase';
 
-
-ReactDOM.render(<App />, getElementById('root'));
+ReactDOM.render(
+    <FirebaseContext.Provider value = {{ firebase, FieldValue }}>
+    <App />,
+    </FirebaseContext.Provider>,
+    document.getElementById('root')
+);
 
 
 
--- DaGram-React/dagram/src/lib/firebase.js
+++ DaGram-React/dagram/src/lib/firebase.js
@@ -4,9 +4,20 @@ import 'firebase/auth'
 
 //Import seed file,
 
-const config {};
+const config {
+    apiKey: "AIzaSyBv540S56U6-jxxAl8EgeFVzYrXhN4IxRk",
+    authDomain: "dagram-react.firebaseapp.com",
+    projectId: "dagram-react",
+    storageBucket: "dagram-react.appspot.com",
+    messagingSenderId: "670352114563",
+    appId: "1:670352114563:web:ba5dfab4cb4d74a632acb6"
+};
 
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
 
 //Run seed, only once!
+// seedDatabase(firebase);
+console.log('firebase', firebase);
+
+export { firebase, FieldValue };
