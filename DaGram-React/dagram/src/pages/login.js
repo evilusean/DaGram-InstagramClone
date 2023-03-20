4f2b52edd453df0eaed27aa7c4a33bb22e5ac07b
evilusean <shawntemesy@hotmail.com>
2023-03-20 19:37:40 -0300

    fixed bug that wouldnt allow package.json to work or react-scripts
    also changed sleep in react-script:start
    removed eslint
    deleted node modules and pack-lock.json
    reinstalled with npm install
    hours later, works now

--- DaGram-React/dagram/src/pages/login.js
+++ DaGram-React/dagram/src/pages/login.js
@@ -1,12 +1,12 @@
-import { useHistory } from "react-router-dom";
-// uses history to get user information
+import { useNavigate } from "react-router-dom";
+// uses history to get user information, had to change from useHistory to useNavigate, useHistory was not working
 import FirebaseContext from '../context/firebase';
 // used to get user information from firebase firestore database
 import { useState, useContext, useEffect } from 'react';
 //use State for storing values in a state
 
 export default function Login() {
-    const history = useHistory();
+    const history = useNavigate();
     const { firebase } = useContext(FirebaseContext);
     // gets information about users, log in, does a user already exist,
     const [emailAddress, SetEmailAddress] = useState('');
@@ -14,7 +14,7 @@ export default function Login() {
     const [password, setPassword] = useState('');
     //gets password for state, and saves it in the state
     const [error, setError] = useState('');
-    const isInvalid = password == '' || emailAddress == '';
+    const isInvalid = password === '' || emailAddress === '';
     //checks if email or password is empty, throws an error isInvalid if they are
     const handleLogin = () => {};
     useEffect(() => {
