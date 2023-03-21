612ec8217749ff50ce0a6422579b90a02428ab50
evilusean <shawntemesy@hotmail.com>
2023-03-21 11:33:13 -0300

    created new dagram2 and added all base dependancies with src files

---
+++ DaGram-React/dagram2/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <p> Hello DaGram</p>
+  );
+}
+
+export default App;
---
+++ DaGram-React/dagram2/src/constants/routes.js
@@ -0,0 +1,9 @@
+export const DASHBOARD = '/';
+//DASHBOARD is the default main page
+export const LOGIN = '/login';
+//routes.js is used to create constant variables that can be used for all pages
+export const SIGN_UP = '/signup';
+export const PROFILE = '/p/:username';
+//:username will be replaced
+export const NOT_FOUND = '/not-found';
+//sends back an error code from a client side app, HTTP Error 404
