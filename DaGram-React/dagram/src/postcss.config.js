07f9768414bc4183f1abd9e93befc9da024ee7f9
evilusean <shawntemesy@hotmail.com>
2023-03-21 18:54:54 -0300

    fcking broke again

--- DaGram-React/dagram/src/postcss.config.js
+++ DaGram-React/dagram/src/postcss.config.js
@@ -1,3 +1,14 @@
+// module.exports = {
+//     plugins: [require('tailwindcss'), require('autoprefixer')]
+// };
+
+const path = require('path');
+
 module.exports = {
-    plugins: [require('tailwindcss'), require('autoprefixer')]
+  plugins: {
+    tailwindcss: {
+      config: path.join(__dirname, 'tailwind.config.js'),
+    },
+    autoprefixer: {},
+  },
 };
