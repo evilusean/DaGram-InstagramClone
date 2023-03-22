5e7bd722e23b68f3f9998666033fa2c3bf6b39b8
evilusean <shawntemesy@hotmail.com>
2023-03-22 16:40:18 -0300

    still broken, new project time

--- DaGram-React/dagram/src/index.js
+++ DaGram-React/dagram/src/index.js
@@ -1,13 +1,27 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import FirebaseContext from './context/firebase';
+// import { firebase, FieldValue } from './lib/firebase';
+// import './styles/app.css';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+//     <App />
+//   </FirebaseContext.Provider>
+// );
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import FirebaseContext from './context/firebase';
 import { firebase, FieldValue } from './lib/firebase';
 import './styles/app.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <FirebaseContext.Provider value={{ firebase, FieldValue }}>
     <App />
-  </FirebaseContext.Provider>
+  </FirebaseContext.Provider>,
+  document.getElementById('root')
 );
