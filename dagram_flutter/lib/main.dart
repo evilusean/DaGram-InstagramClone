de4e9cbec1cd8420a5800a5e1b29125d9b707343
evilusean <shawntemesy@hotmail.com>
2023-03-24 18:04:02 -0300

    added firebase to main.dart
    nothing broke

--- dagram_flutter/lib/main.dart
+++ dagram_flutter/lib/main.dart
@@ -3,8 +3,11 @@ import 'responsive/responsive_layout_screen.dart';
 import 'package:dagram_flutter/utils/colors.dart';
 import 'package:dagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:dagram_flutter/responsive/web_screen_layout.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
