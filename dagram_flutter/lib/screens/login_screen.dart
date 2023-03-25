c555528db3d1943772e2b3bc9c561b383786b874
evilusean <shawntemesy@hotmail.com>
2023-03-24 22:19:41 -0300

    added login text boxes

--- dagram_flutter/lib/screens/login_screen.dart
+++ dagram_flutter/lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dagram_flutter/widgets/text_field_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:dagram_flutter/utils/colors.dart';
@@ -10,6 +11,15 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,13 +30,32 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Flexible(child: Container(), flex: 2,),
               SvgPicture.asset(
                 'assets/DaGram.svg', 
                 color: primaryColor,
                  height: 64,
                  ),
               //text field input for email
+              const SizedBox(height: 64),
+              TextFieldInput(
+                hintText: 'Enter your Email.',
+                textInputType: TextInputType.emailAddress,
+                textEditingController: _emailController,
+                ),
               // text field for password
+              const SizedBox(
+                height: 24,
+              ),
+              TextFieldInput(
+                hintText: 'Enter your Password.',
+                textInputType: TextInputType.text,
+                textEditingController: _passwordController,
+                isPass: true,
+                ),
+              const SizedBox(
+                height: 24,
+              ),
               //button login
               // signup
             ],)
