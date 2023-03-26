96a6f41dcd7fd440f1c7e227c524783bf84c9bd5
evilusean <shawntemesy@hotmail.com>
2023-03-26 00:06:33 -0300

    finished authentication setup firebase firestore

--- dagram_flutter/lib/resources/auth_methods.dart
+++ dagram_flutter/lib/resources/auth_methods.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:typed_data';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   // sign up user
   Future<String> signUpUser({
     required String email,
@@ -12,9 +15,24 @@ class AuthMethods {
     required String bio,
     required Uint8List file,
   }) async {
-      String res = "Some error occured."
+      String res = "Some error occured.";
       try {
-        if()
+        if(email.isNotEmpty || password.isNotEmpty || username.isNotEmpty || bio.isNotEmpty || file != null){
+          // Register User
+          UserCredential cred = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+
+          print(cred.user!.uid);
+          // Add User to Firestore database
+          await _firestore.collection('users').doc(cred.user!.uid).set({
+            "username": username,
+            "uid": cred.user!.uid,
+            "email": email,
+            "bio": bio,
+            'followers': [],
+            'following': [],
+          });
+          res = "Success!";
+        }
       } catch(err) {
         res = err.toString();
       }
