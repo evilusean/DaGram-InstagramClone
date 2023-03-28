09948cbfbbabb190e712efdc0bb978d8e3f48638
evilusean <shawntemesy@hotmail.com>
2023-03-28 00:42:42 -0300

    started mobile screen layout

--- dagram_flutter/lib/responsive/mobile_screen_layout.dart
+++ dagram_flutter/lib/responsive/mobile_screen_layout.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
-//import 'package:dagram_flutter/utils/colors.dart';
+import 'package:dagram_flutter/utils/colors.dart';
 
-class MobileScreenLayout extends StatelessWidget {
+class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
+
+  @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+
+  class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+    String username = "";
+    
+    @override
+    void initState() {
+      super.initState();
+      getUsername();
+    }
+
+    void GetUsername() async {
+      DocumentSnapshoy snap = FirebaseFirestore.instance
+        .collection('users')
+        .doc(Firebase data()
+        .get();
+
+    print(snap.data())
+    };
+  }
 
   @override
   Widget build(BuildContext context) {
