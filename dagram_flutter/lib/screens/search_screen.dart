dd4c70a1be88c452380df5070b2639a8d9177a79
evilusean <shawntemesy@hotmail.com>
2023-03-31 21:49:04 -0300

    finished user search function

--- dagram_flutter/lib/screens/search_screen.dart
+++ dagram_flutter/lib/screens/search_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dagram_flutter/utils/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,7 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   final TextEditingController searchController = TextEditingController();
+  bool isShowUsers = false;
 
   @override
   void dispose() {
@@ -28,10 +30,38 @@ class _SearchScreenState extends State<SearchScreen> {
             labelText: 'Search For a User.'
           ),
           onFieldSubmitted: (String _) {
-            print(_);
+            setState(() {
+              isShowUsers = true;
+            });
           },
-        )
-      )
+        ),
+      ),
+      body: isShowUsers? FutureBuilder(
+        future: FirebaseFirestore.instance.collection('users')
+        .where('username', isGreaterThanOrEqualTo: searchController.text,)
+        .get(),
+      builder: (context, snapshot) {
+        if(!snapshot.hasData) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return ListView.builder(
+          itemCount: (snapshot.data! as dynamic).docs.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  (snapshot.data! as dynamic).docs(index)['photoUrl']
+                ),
+              ),
+              title: Text(
+                (snapshot.data! as dynamic).docs[index]['username'],),
+            );
+          }
+          );
+      },
+      ): Text('Posts'),
     );
   }
 }
